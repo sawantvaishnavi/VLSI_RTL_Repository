1c0570e6be74ce55e2789621315c773707256d02
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-12-23 18:44:33 +0530

    Update 1.Single Port RAM with Synchronous Read-Write.v

--- Memory/1.Single Port RAM with Synchronous Read-Write.v
+++ Memory/1.Single Port RAM with Synchronous Read-Write.v
@@ -1,2 +1,32 @@
+//A single-port RAM has only one data port, which means that read and write operations cannot occur simultaneously at different addresses. If a write operation is in progress, a read operation must wait, and vice versa.
+//In clocked always blocks, missing else means HOLD, not LATCH.
+//Latches are inferred only in combinational blocks like always @(*), not in edge-triggered blocks.
 
-#A single-port RAM has only one data port, which means that read and write operations cannot occur simultaneously at different addresses. If a write operation is in progress, a read operation must wait, and vice versa.
+module Single_Port_RAM ( 
+  input clk, 
+  input rd, 
+  input wr, 
+  input cs,
+  input [9:0] addr,
+  input [7:0] wdata,
+  output reg [7:0] rdata
+   );
+  
+  reg [7:0] mem [1023:0] ;
+ 
+  
+  always@(posedge clk)
+    if(cs) begin
+      
+       //write operation
+      if (wr && !rd)
+        mem[addr] <= data;
+  
+      //read operation
+      if(rd && !wr)
+        d_out <= mem [addr];
+  
+    end
+  
+  
+endmodule
