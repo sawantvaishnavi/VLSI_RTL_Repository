8e81681bb5239d2240da568c4943bcdd63683118
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2025-12-23 17:37:04 +0530

    Update and rename 1.Single Port RAM.v to 1.Single Port RAM with Synchronous Read-Write.v

---
+++ Memory/1.Single Port RAM with Synchronous Read-Write.v
@@ -0,0 +1,2 @@
+
+#A single-port RAM has only one data port, which means that read and write operations cannot occur simultaneously at different addresses. If a write operation is in progress, a read operation must wait, and vice versa.
