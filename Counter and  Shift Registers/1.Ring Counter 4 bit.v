9c37b3b1f9750a78a131c96563e2774ca9527826
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-02-03 21:34:13 +0530

    Update 1.Ring Counter 4 bit.v

--- Counter and  Shift Registers/1.Ring Counter 4 bit.v
+++ Counter and  Shift Registers/1.Ring Counter 4 bit.v
@@ -1,3 +1,4 @@
+////////// Method 1 //////////
 module ring_counter (input clk, clr, output reg [3:0]q);
   
   always@(posedge clk) begin
@@ -38,5 +39,65 @@ module tb ();
     $dumpfile("dump.vcd");
     $dumpvars(1);
     $monitor ("clk=%b, clr=%b,q=%b",clk,clr,q);
+  end
+endmodule
+
+
+
+////////// Method 2 //////////
+module ring_counter (input clk, clr, output reg [3:0] count);
+  
+  always@(posedge clk)
+    begin
+      if (clr) 
+        count =  4'b1000;
+      else
+        count = {count[2:0], count[3]};    
+    end
+  
+endmodule
+
+
+////////// Method 3 //////////
+module ring_counter (input clk, clr, output reg [3:0] count);
+  
+  always@(posedge clk)
+    begin
+      if (clr) 
+        count <=  4'b1000;
+      else begin
+       count <= count << 1;
+       count[0] <= count[3];
+      end
+    
+    end
+  
+endmodule
+
+////////// Tb for m2 and m3 //////////
+module tb;
+  reg clk;
+  reg clr;
+  wire [3:0] count;
+  
+  ring_counter dut (clk, clr, count);
+  
+  initial clk=0;
+  always #10 clk = ~clk;
+  
+  initial begin
+   
+    clr = 1;
+    #20 clr = 0;
+  end
+  
+  initial begin 
+    #300 $finish;
+  end 
+  
+  initial begin 
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    $monitor (" time= %0t, count=%b",$time,count);
   end
 endmodule
